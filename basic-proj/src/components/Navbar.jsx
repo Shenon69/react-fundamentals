5a0826a38b24ecb33b096eefb39fe998507db3df
Shenon69 <ask.tikki69@gmail.com>
2023-02-18 17:25:10 +0530

    boostrap added

---
+++ basic-proj/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='navbar bg-dark shadow' data-bs-theme='dark'>
+       <span className='navbar-brand mb-2 h1'>Name List</span> 
+    </nav>
+  )
+}
+
+export default Navbar;
